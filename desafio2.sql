7a4b67236eb45a4aea77eca76054f047bbf01bf7
Diego Silva <coxcorp@gmail.com>
2022-02-04 15:20:09 -0300

    desafio2

---
+++ desafio2.sql
@@ -0,0 +1,8 @@
+SELECT 
+  COUNT(DISTINCT can.cancao_id) AS cancoes,
+  COUNT(DISTINCT art.artista_id) AS artistas,
+  COUNT(DISTINCT alb.album_id) AS albuns
+FROM
+  SpotifyClone.cancoes AS can,
+  SpotifyClone.artistas AS art,
+  SpotifyClone.albuns AS alb;
